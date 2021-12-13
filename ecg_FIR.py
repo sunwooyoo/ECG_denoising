040d93ff776c150d44ea80fa6155d3d2f23412b0
sunwooyoo <70518295+sunwooyoo@users.noreply.github.com>
2021-12-13 15:09:07 +0900

    Update ecg_FIR.py

--- ecg_FIR.py
+++ ecg_FIR.py
@@ -37,7 +37,7 @@ parser.add_argument('-order','--order', type = int)
 args = parser.parse_args()
 
 
-path = '/home/swyoo/bnn_ISOCC/mitbih_database/'
+path = '../mitbih_database/'
 csv_path = path + '115.csv'
 annotation_path = path + '115annotations.txt'
 df = pd.read_csv(csv_path,)
