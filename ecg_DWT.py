52f2c33d165922ceffa0cdf71182537e5695a3d7
sunwooyoo <70518295+sunwooyoo@users.noreply.github.com>
2024-12-01 16:11:50 +0900

    Update ecg_DWT.py

--- ecg_DWT.py
+++ ecg_DWT.py
@@ -11,8 +11,8 @@ args = parser.parse_args()
 
 
 path = './dataset/'
-csv_path = path + '106.csv'
-annotation_path = path + '106annotations.txt'
+csv_path = path + '119.csv'
+annotation_path = path + '119annotations.txt'
 df = pd.read_csv(csv_path,)
 # Get data:
 data = df["'MLII'"].values
